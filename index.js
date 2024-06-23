fc083541ab2692edf62b3c2c952c2d526de036cf
Aman <107303095+Amn9909@users.noreply.github.com>
2024-06-23 18:37:30 +0530

    added change for http module

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require("http")
+const fs = require('fs')
 
 http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>Server is up</h1>')
+    const ip = req.connection.remoteAddress
+    let log = `${Date.now()} : new req ${ip}\n`
+    fs.appendFile('logs.txt', log, (err, data) => {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>Server is up</h1>')
+    })
 }).listen(9898)
